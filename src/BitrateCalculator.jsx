4cbceb50c47434a4a98daf0cc4ec3f55984f1c0e
Justin Love <git@JustinLove.name>
2019-04-08 11:27:05 -0500

    extract resolution control

--- src/BitrateCalculator.jsx
+++ src/BitrateCalculator.jsx
@@ -1,23 +1,8 @@
 import {BitrateControl} from './BitrateControl.js'
+import {ResolutionControl, resolutionOptions} from './ResolutionControl.js'
 import {FramerateControl} from './FramerateControl.js'
 
 'use strict'
-
-var resolutionOptions = [
-  {w: 640, h: 360},
-  {w: 969, h: 392},
-  {w: 768, h: 432},
-  {w: 852, h: 480},
-  {w: 960, h: 540},
-  {w: 1096, h: 616},
-  {w: 1152, h: 648},
-  {w: 1280, h: 720},
-  {w: 1140, h: 810},
-  {w: 1536, h: 864},
-  {w: 1600, h: 900},
-  {w: 1920, h: 1080}
-]
-resolutionOptions.forEach(function(res) { res.value = `${res.w}x${res.h}`})
 
 var calculateBpp = function(state) {
   return state.bitrate * 1000 / (state.resolution.w * state.resolution.h * state.framerate)
@@ -41,14 +26,11 @@ export class BitrateCalculator extends React.Component {
     })
   }
 
-  changedResolution(e) {
-    var res = resolutionOptions.find(function(res) {return res.value == e.target.value})
-    if (res) {
-      this.setState((prevState) => {
-        prevState.resolution = res
-        return {resolution: res, bpp: calculateBpp(prevState)}
-      })
-    }
+  changedResolution(res) {
+    this.setState((prevState) => {
+      prevState.resolution = res
+      return {resolution: res, bpp: calculateBpp(prevState)}
+    })
   }
 
   changedFramerate(framerate) {
@@ -67,19 +49,11 @@ export class BitrateCalculator extends React.Component {
             onChange={this.changedBitrate.bind(this)}
             >
         </BitrateControl>
-        <div>
-          <label htmlFor='resolution'>Resolution</label>{' '}
-          <select
-            id='resolution'
-            name='resolution'
-            value={this.state.resolution.value}
+        <ResolutionControl
+            value={this.state.resolution}
             onChange={this.changedResolution.bind(this)}
             >
-            {resolutionOptions.map(function(res) {
-              return <option value={res.value} key={res.value}>{res.value}</option>
-            })}
-          </select>
-        </div>
+        </ResolutionControl>
         <FramerateControl
             value={this.state.framerate}
             onChange={this.changedFramerate.bind(this)}
---
+++ src/ResolutionControl.jsx
@@ -0,0 +1,46 @@
+'use strict'
+
+export var resolutionOptions = [
+  {w: 640, h: 360},
+  {w: 969, h: 392},
+  {w: 768, h: 432},
+  {w: 852, h: 480},
+  {w: 960, h: 540},
+  {w: 1096, h: 616},
+  {w: 1152, h: 648},
+  {w: 1280, h: 720},
+  {w: 1140, h: 810},
+  {w: 1536, h: 864},
+  {w: 1600, h: 900},
+  {w: 1920, h: 1080}
+]
+resolutionOptions.forEach(function(res) { res.value = `${res.w}x${res.h}`})
+
+export class ResolutionControl extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  changedResolution(e) {
+    var res = resolutionOptions.find(function(res) {return res.value == e.target.value})
+    if (res) {
+      this.props.onChange(res)
+    }
+  }
+
+  render() {
+    return <div>
+      <label htmlFor='resolution'>Resolution</label>{' '}
+      <select
+        id='resolution'
+        name='resolution'
+        value={this.props.value.value}
+        onChange={this.changedResolution.bind(this)}
+        >
+        {resolutionOptions.map(function(res) {
+          return <option value={res.value} key={res.value}>{res.value}</option>
+        })}
+      </select>
+    </div>
+  }
+}
