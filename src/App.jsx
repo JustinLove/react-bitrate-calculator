ed1ab6104c5b2fe6eebfa3db81c6f0025c326c60
Justin Love <git@JustinLove.name>
2019-05-21 14:26:08 -0500

    help text

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,12 @@ export class App extends React.Component {
   render() {
     return <div>
       <BitrateCalculator />
+      <p>
+        BPP: Bits Per Pixel. <a href="https://docs.google.com/spreadsheets/d/1Vm0_8BQGNxKcowK5RwTgiqQisR4mVblbWvl-N4A-lDM/edit#gid=0">0.1 is generally recommended</a> for streaming, perhaps 0.6 for low motions streams.
+      </p>
+      <p>
+        BPI: Bits Per Inch. My own unit, that takes into account display size on my monitor. I find my settings of choice are 5-9.
+      </p>
     </div>
   }
 }
